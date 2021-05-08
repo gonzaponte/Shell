9aa556eef702c31d2e29c9c4d8416437592187f5
Gonzalo Martinez Lema <gonzaponte@gmail.com>
2021-05-08 13:48:59 +0300

    Add cmake to g4 setup

--- Shira/setups/geant4.sh
+++ Shira/setups/geant4.sh
@@ -1,4 +1,8 @@
-export G4DIR=$SWDIR/geant4.10.06.p01/install
+
+setup cmake
+
+
+export G4DIR=$SWDIR/geant4.10.07.p01-install
 export CMAKE_PREFIX_PATH=$G4DIR:${CMAKE_PREFIX_PATH}
 
 source $G4DIR/bin/geant4.sh

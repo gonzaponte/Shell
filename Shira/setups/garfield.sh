6a5cdbe06dd9cde5b0c284ab3f560ba4d0308f61
Gonzalo Martinez Lema <gonzaponte@gmail.com>
2021-11-22 18:09:44 +0200

    Add garfield config

--- Shira/setups/garfield.sh
+++ Shira/setups/garfield.sh
@@ -1,5 +1,9 @@
 setup root
 setup geant4
+setup xerces
 
-export GARFIELD_HOME=$SWDIR/garfield
+
+export GARFIELD_HOME=$SWDIR/garfieldpp
 export CMAKE_PREFIX_PATH=$GARFIELD_HOME/install:${CMAKE_PREFIX_PATH}
+
+source ${GARFIELD_HOME}/install/share/Garfield/setupGarfield.sh

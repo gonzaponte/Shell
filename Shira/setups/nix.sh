818b3b3dd3e67cf4d2bfeeac44d32b4d3fc26050
Gonzalo Martinez Lema <gonzaponte@gmail.com>
2022-06-04 17:16:14 +0300

    Update nix setup

--- Shira/setups/nix.sh
+++ Shira/setups/nix.sh
@@ -1,4 +1,11 @@
-. $HOME/.nix-profile/etc/profile.d/nix.sh
+# Nix
+if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
+  . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+fi
+# End Nix
+
+####OLD
+#. $HOME/.nix-profile/etc/profile.d/nix.sh
 
 eval "$(direnv hook bash)"
 
